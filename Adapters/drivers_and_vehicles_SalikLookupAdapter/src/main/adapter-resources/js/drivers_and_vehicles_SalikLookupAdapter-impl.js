eb97291d0ca48b7a1eabba6db793b90cfb980831
Balaji Sabdhar M <bmuthukr@in.ibm.com>
2020-08-12 15:54:55 +0530

    DEBUG

--- Adapters/drivers_and_vehicles_SalikLookupAdapter/src/main/adapter-resources/js/drivers_and_vehicles_SalikLookupAdapter-impl.js
+++ Adapters/drivers_and_vehicles_SalikLookupAdapter/src/main/adapter-resources/js/drivers_and_vehicles_SalikLookupAdapter-impl.js
@@ -569,15 +569,19 @@ function getPlateCodeLookup(isEncryptResponse, encryptionPassword) {
 
 function getPlateLookup(isEncryptResponse, encryptionPassword) {
     MFP.Logger.info("getPlateLookup Start");
-    var userName = MFP.Server.getPropertyValue("wsse.tibco.username");
-	var password = MFP.Server.getPropertyValue("wsse.tibco.password");
+    var userName1 = MFP.Server.getPropertyValue("wsse.tibco.username");
+	var password1 = MFP.Server.getPropertyValue("wsse.tibco.password");
+	MFP.Logger.info("userName1:" + userName1 + "||||");
+	MFP.Logger.info("password1:" + password1 + "||||");
+	var userName = "plateUsername";
+	var password = "platePassword";
     
     var request = '<soapenv:Envelope xmlns:sch="http://www.rta.ae/schemas/SalikLookupService/Schema.xsd" xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/">'+
          '<soapenv:Header>'+
          '<wsse:Security soapenv:mustUnderstand="1" xmlns:wsse="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd" xmlns:wsu="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd">'+
          '<wsse:UsernameToken wsu:Id="UsernameToken-13">'+
-         '<wsse:Username>testUser</wsse:Username>'+
-         '<wsse:Password Type="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-username-token-profile-1.0#PasswordText">test</wsse:Password>'+
+         '<wsse:Username>' + userName + '</wsse:Username>'+
+         '<wsse:Password Type="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-username-token-profile-1.0#PasswordText">' + password + '</wsse:Password>'+
          '<wsse:Nonce EncodingType="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-soap-message-security-1.0#Base64Binary">tCHN7DaNqD2yifNCpNnyGg==</wsse:Nonce>'+
          '<wsu:Created>2017-06-13T11:51:02.107Z</wsu:Created>'+
          '</wsse:UsernameToken>'+
