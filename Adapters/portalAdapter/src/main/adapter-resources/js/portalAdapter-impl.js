397d0f92e53635dcc1bfd615f827e3dd9f1ef04a
AymanNagehMarie <35450427+AymanNagehMarie@users.noreply.github.com>
2020-08-16 12:19:12 +0400

    change text to be plain get profile

--- Adapters/portalAdapter/src/main/adapter-resources/js/portalAdapter-impl.js
+++ Adapters/portalAdapter/src/main/adapter-resources/js/portalAdapter-impl.js
@@ -310,7 +310,7 @@ function invokeWebService2(body) {
 	try {
 		var input = {
 			method: 'post',
-			returnedContentType: 'text',
+			returnedContentType: 'plain',
 			path: WSDL_Path_For_GetUserProfile ,
 			body: {
 				content: body.toString(),
