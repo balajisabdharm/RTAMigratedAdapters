fef636f4a612ac8fb649e46e308c390aa0ee8754
amitgoyal846 <vickygoyal846@gmail.com>
2020-08-16 20:48:09 +0530

    Update drivers_and_vehicles_SalikLookupAdapter-impl.js

--- Adapters/drivers_and_vehicles_SalikLookupAdapter/src/main/adapter-resources/js/drivers_and_vehicles_SalikLookupAdapter-impl.js
+++ Adapters/drivers_and_vehicles_SalikLookupAdapter/src/main/adapter-resources/js/drivers_and_vehicles_SalikLookupAdapter-impl.js
@@ -386,8 +386,8 @@ function invokeWebServiceString(request, servicePath, SOAPAction, isEncryptRespo
 
 
     var webServiceResult = MFP.Server.invokeHttp(input);
-    var _result_truncated =JSON.stringify(webServiceResult)
-    MFP.Logger.info("Response   without modification" +result_truncated);
+    var _result_truncated =JSON.stringify(webServiceResult);
+    MFP.Logger.info("Response   without modification" +_result_truncated);
     //Code changed by Amit Goyal ...response is returned without DeleteCredentials and encryptData functionality
     var s = _result_truncated.replace(/[\n\t\r]/g,' ');
     return JSON.parse(_result_truncated);
