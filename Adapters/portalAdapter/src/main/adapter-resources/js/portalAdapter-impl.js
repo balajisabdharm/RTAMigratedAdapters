d070fc2964b7b82c9bac1c76685e34a01e21b1a9
amitgoyal846 <vickygoyal846@gmail.com>
2020-08-16 21:57:11 +0530

    Update portalAdapter-impl.js

--- Adapters/portalAdapter/src/main/adapter-resources/js/portalAdapter-impl.js
+++ Adapters/portalAdapter/src/main/adapter-resources/js/portalAdapter-impl.js
@@ -441,7 +441,7 @@ function getUserProfile(uid, appid) {
 			 xmlns:sch="http://www.rta.ae/ActiveMatrix/ESB/schemas/PortalProfileService/Schema.xsd"
 			 xmlns:wsse="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd"'
 			 xmlns:wsu="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd">
-			 getSoapHeader()
+        {getSoapHeader()}
 			<soapenv:Body><sch:getUserProfile><sch:userId>
              {uid}
 			 </sch:userId><sch:applicationId>
