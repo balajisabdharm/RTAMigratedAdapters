f4f78fca08778e80bd5a16563df4793e43a91a3b
amitgoyal846 <vickygoyal846@gmail.com>
2020-08-18 23:07:11 +0530

    Update drivers_and_vehicles_BookVehicleIncpectionAdapter-impl.js

--- Adapters/drivers_and_vehicles_BookVehicleIncpectionAdapter/src/main/adapter-resources/js/drivers_and_vehicles_BookVehicleIncpectionAdapter-impl.js
+++ Adapters/drivers_and_vehicles_BookVehicleIncpectionAdapter/src/main/adapter-resources/js/drivers_and_vehicles_BookVehicleIncpectionAdapter-impl.js
@@ -56,10 +56,10 @@ function getGlobalServices(vehicleType, isEncryptResponse, encryptionPassword){
 
 function getAvailableAppointmentCenters (isEncryptResponse, encryptionPassword){
 	var bodyRequest ="<sch:getAvailableAppointmentCentersRequest/>";
-    MFP.Logger.info("getAvailableAppointmentCenters "+ bodyRequest);
+    MFP.Logger.info("getAvailableAppointmentCenters >>>>>> "+ bodyRequest);
 	var request = getRequestString(bodyRequest);
 
-    var requestObj = buildBody([JSON.stringify(request)], true);
+    var requestObj = buildBody([request], true);
 	MFP.Logger.warn("getAvailableAppointmentCenters request | " + requestObj);
 	var result = invokeWebServiceString(requestObj, isEncryptResponse,
 			encryptionPassword);
@@ -307,7 +307,7 @@ function getAppointmentBasket(appointmentId,isEncryptResponse, encryptionPasswor
 
 function buildBody(parameters, isStatic) {
 	var request = "";
-
+MFP.Logger.info("buildBody parameters"+parameters);
 	if (isStatic == true) {
 		request = MFP.Server.invokeProcedure({
 			adapter : 'drivers_and_vehciles_utilitiesAdapter',
