b54b8ae9637347b3c06b7ca76849bb90783310dc
fjchawla <69506524+fjchawla@users.noreply.github.com>
2020-08-18 13:10:29 +0400

    WL to MFP

--- Adapters/GreenPointsDbAdapter/src/main/adapter-resources/js/GreenPointsDbAdapter-impl.js
+++ Adapters/GreenPointsDbAdapter/src/main/adapter-resources/js/GreenPointsDbAdapter-impl.js
@@ -11,7 +11,7 @@
 
 var DB_TABLES = {
 	'GreenPoints' : {
-		getLatest_PT_ForAllUsers_And_InsertOrUpdate_Into_GreenPointsAgg : WL.Server
+		getLatest_PT_ForAllUsers_And_InsertOrUpdate_Into_GreenPointsAgg : MFP.Server
 			.createSQLStatement('MERGE INTO GREEN_POINTS_AGG dest '
 						+'	USING( SELECT USER_ID , '
 						+'			USER_NAME , '
