9b0bcb5213492256adbe6e657f5bdb818083b643
amitgoyal846 <vickygoyal846@gmail.com>
2020-08-19 00:06:19 +0530

    Update drivers_and_vehicles_BookVehicleIncpectionAdapter-impl.js

--- Adapters/drivers_and_vehicles_BookVehicleIncpectionAdapter/src/main/adapter-resources/js/drivers_and_vehicles_BookVehicleIncpectionAdapter-impl.js
+++ Adapters/drivers_and_vehicles_BookVehicleIncpectionAdapter/src/main/adapter-resources/js/drivers_and_vehicles_BookVehicleIncpectionAdapter-impl.js
@@ -59,7 +59,8 @@ function getAvailableAppointmentCenters (isEncryptResponse, encryptionPassword){
     MFP.Logger.info("getAvailableAppointmentCenters >^^^>>>>> "+ bodyRequest);
 	var request = getRequestString(bodyRequest);
 
-    var requestObj = buildBody([JSON.stringify(request)], true);
+    //var requestObj = buildBody([JSON.stringify(request)], true);
+    var requestObj = buildBody((request)], true);
 	MFP.Logger.warn("getAvailableAppointmentCenters request | " + requestObj);
 	var result = invokeWebServiceString(JSON.parse(requestObj), isEncryptResponse,
 			encryptionPassword);
@@ -348,12 +349,12 @@ function getRequestString(bodyString) {
 		+ '</soapenv:Body>' + '</soapenv:Envelope>';
     MFP.Logger.info("getRequestString "+request);
 	return request;
-}
+//}
 
 function invokeWebServiceString(request, isEncryptResponse, encryptionPassword) {
 	MFP.Logger.warn("invokeWebServiceString request >>>" +request);
     
-    MFP.Logger.warn("invokeWebServiceString request in json format >>>>>" +JSON.parse(request));
+    //MFP.Logger.warn("invokeWebServiceString request in json format >>>>>" +JSON.parse(request));
 	var input = {
 			method : 'post',
 			headers : {
