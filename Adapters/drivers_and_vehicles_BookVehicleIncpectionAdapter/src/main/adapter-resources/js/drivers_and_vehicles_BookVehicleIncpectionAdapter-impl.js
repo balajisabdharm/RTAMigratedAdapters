31dc06d8c68a12a299cc650e1f811d303ec2568f
Balaji Sabdhar M <bmuthukr@in.ibm.com>
2020-08-19 22:14:17 +0530

    Issue Fix

--- Adapters/drivers_and_vehicles_BookVehicleIncpectionAdapter/src/main/adapter-resources/js/drivers_and_vehicles_BookVehicleIncpectionAdapter-impl.js
+++ Adapters/drivers_and_vehicles_BookVehicleIncpectionAdapter/src/main/adapter-resources/js/drivers_and_vehicles_BookVehicleIncpectionAdapter-impl.js
@@ -354,7 +354,7 @@ function getRequestString(bodyString) {
 }
 
 function invokeWebServiceString(request, isEncryptResponse, encryptionPassword) {
-	request='<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:sch=\"http://www.rta.ae/ActiveMatrix/ESB/AppointmentService/XMLSchema/Schema.xsd\"><soapenv:Header><wsse:Security soapenv:mustUnderstand=\"0\" xmlns:wsse=\" http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd\" xmlns=\"http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd\" xmlns:sch=\"http://schemas.xmlsoap.org/soap/envelope/\"><wsse:UsernameToken><wsse:Username>Mobstguser</wsse:Username><wsse:Password>m792!du)+1g</wsse:Password></wsse:UsernameToken></wsse:Security></soapenv:Header><soapenv:Body><sch:getAvailableAppointmentCentersRequest/></soapenv:Body></soapenv:Envelope>';
+	request='<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:sch="http://www.rta.ae/ActiveMatrix/ESB/AppointmentService/XMLSchema/Schema.xsd"><soapenv:Header><wsse:Security soapenv:mustUnderstand="0" xmlns:wsse="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd" xmlns="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd" xmlns:sch="http://schemas.xmlsoap.org/soap/envelope/"><wsse:UsernameToken><wsse:Username>Mobstguser</wsse:Username><wsse:Password>m792!du)+1g</wsse:Password></wsse:UsernameToken></wsse:Security></soapenv:Header><soapenv:Body><sch:getAvailableAppointmentCentersRequest/></soapenv:Body></soapenv:Envelope>';
 	MFP.Logger.warn("invokeWebServiceString request >>>" +request);
    
 	var input = {
