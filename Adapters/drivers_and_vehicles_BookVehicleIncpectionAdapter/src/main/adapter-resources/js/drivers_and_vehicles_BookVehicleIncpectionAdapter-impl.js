656bc3d4c80681268c1742f706284e45c951abac
Balaji Sabdhar M <bmuthukr@in.ibm.com>
2020-08-19 20:27:09 +0530

    Issue fix

--- Adapters/drivers_and_vehicles_BookVehicleIncpectionAdapter/src/main/adapter-resources/js/drivers_and_vehicles_BookVehicleIncpectionAdapter-impl.js
+++ Adapters/drivers_and_vehicles_BookVehicleIncpectionAdapter/src/main/adapter-resources/js/drivers_and_vehicles_BookVehicleIncpectionAdapter-impl.js
@@ -60,7 +60,7 @@ function getAvailableAppointmentCenters (isEncryptResponse, encryptionPassword){
 	var request = getRequestString(bodyRequest);
 
     //var requestObj = buildBody([JSON.stringify(request)], true);
-	var requestObj = buildBody([(request)], true);
+	var requestObj = buildBody([request], true);
     MFP.Logger.warn("getAvailableAppointmentCenters request | " + requestObj);
 	//var result = invokeWebServiceString(JSON.stringify(requestObj), isEncryptResponse,encryptionPassword);
     var result = invokeWebServiceString((requestObj), isEncryptResponse,
