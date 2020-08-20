90897bbf42a62cbbbca348086cfa78b95b595001
amitgoyal846 <vickygoyal846@gmail.com>
2020-08-20 22:50:17 +0530

    Update drivers_and_vehicles_lookupsAdapter-impl.js

--- Adapters/drivers_and_vehicles_lookupsAdapter/src/main/adapter-resources/js/drivers_and_vehicles_lookupsAdapter-impl.js
+++ Adapters/drivers_and_vehicles_lookupsAdapter/src/main/adapter-resources/js/drivers_and_vehicles_lookupsAdapter-impl.js
@@ -165,7 +165,7 @@ function buildBody(parameters, isStatic) {
 
 function invokeWebService(body,targetURL,headers, isEncryptResponse, encryptionPassword) {
 	//var startTime = new Date().getTime();
-
+  MFP.Logger("drivers_and_vehicles_lookupsAdapter invokeWebService called ");
 	if (!headers)
 		headers = {
 			"SOAPAction" : ""
