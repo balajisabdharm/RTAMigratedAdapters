ee7dbe297da7334d7fdeaf7adcdacb9d566452b4
amitgoyal846 <vickygoyal846@gmail.com>
2020-08-20 23:16:53 +0530

    Revert "Update drivers_and_vehicles_lookupsAdapter-impl.js"
    This reverts commit 4e7d061e25d898621a9bb30a96fd703ffe60f321.

--- Adapters/drivers_and_vehicles_lookupsAdapter/src/main/adapter-resources/js/drivers_and_vehicles_lookupsAdapter-impl.js
+++ Adapters/drivers_and_vehicles_lookupsAdapter/src/main/adapter-resources/js/drivers_and_vehicles_lookupsAdapter-impl.js
@@ -165,7 +165,7 @@ function buildBody(parameters, isStatic) {
 
 function invokeWebService(body,targetURL,headers, isEncryptResponse, encryptionPassword) {
 	//var startTime = new Date().getTime();
-
+MFP.Logger("drivers_and_vehicles_lookupsAdapter invokeWebService webServiceResult body"+ body );
 	if (!headers)
 		headers = {
 			"SOAPAction" : ""
@@ -178,8 +178,8 @@ function invokeWebService(body,targetURL,headers, isEncryptResponse, encryptionP
 			path : targetURL,
 			//path:'/TransactionServiceMock',
 			body : {
-				content : body.toString(),
-                
+				//content : body.toString(),
+                content : body,
 				contentType : 'text/xml; charset=utf-8'
 			}
 	};
@@ -189,7 +189,7 @@ function invokeWebService(body,targetURL,headers, isEncryptResponse, encryptionP
 	headers && (input['headers'] = headers);
 
 	var webServiceResult = MFP.Server.invokeHttp(input);
-    MFP.Logger("drivers_and_vehicles_lookupsAdapter invokeWebService webServiceResult ");
+    MFP.Logger("drivers_and_vehicles_lookupsAdapter invokeWebService webServiceResult "+JSON.stringify(webServiceResult) );
 	if(isEncryptResponse != undefined && isEncryptResponse == true)
 	{
 		var responseString = JSON.stringify(webServiceResult);
