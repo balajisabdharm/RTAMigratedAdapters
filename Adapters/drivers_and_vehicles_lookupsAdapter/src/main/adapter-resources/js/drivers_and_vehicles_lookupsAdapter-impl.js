9fd9662a4cfa19c47871ef19655c2c7afce50005
Balaji Sabdhar M <bmuthukr@in.ibm.com>
2020-08-21 09:57:14 +0530

    Revert "Revert "Merge branch 'AdapterTestBranch' of https://github.com/balajisabdharm/RTAMigratedAdapters into AdapterTestBranch""
    This reverts commit 60b460d7995f575a0779bb58c5e9783b7db68e0e.

--- Adapters/drivers_and_vehicles_lookupsAdapter/src/main/adapter-resources/js/drivers_and_vehicles_lookupsAdapter-impl.js
+++ Adapters/drivers_and_vehicles_lookupsAdapter/src/main/adapter-resources/js/drivers_and_vehicles_lookupsAdapter-impl.js
@@ -14,6 +14,8 @@ var userName = "%#credentials!#!username#%";
 var password = "%#credentials!#!password#%";
 var externalUsername = "%#credentials!#!externalUsername#%";
 var externalPassword = "%#credentials!#!externalPassword#%";
+var username_traffic = "%#credentials!#!username_traffic#%";
+var password_traffic = "%#credentials!#!password_traffic#%";
 var IsDebugging;
 function Log(text){
 	try {
@@ -28,10 +30,10 @@ function Log(text){
 		MFP.Logger.debug(text);
 }
 function FFULookupInfoService(params, isEncryptResponse, encryptionPassword){
-	var invocationData = {
+	/*var invocationData = {
 			adapter : 'drivers_and_vehicles_trafficAdapter',
 			procedure : 'FFULookupInfoService',
-			parameters : [params.toString(), isEncryptResponse.toString(), encryptionPassword.toString()]
+			parameters : [params, isEncryptResponse, encryptionPassword]
 	};
 	var response =  MFP.Server.invokeProcedure(invocationData);
 	var invocationData = {
@@ -39,7 +41,26 @@ function FFULookupInfoService(params, isEncryptResponse, encryptionPassword){
 			procedure : 'deleteCredientails',
 			parameters : [response.toString()]
 	};
-	return MFP.Server.invokeProcedure(invocationData); 
+	return MFP.Server.invokeProcedure(invocationData); */
+	
+	
+	var envHeader = { 
+			"ae:authenticationHeader" :{
+				"ae:externalUsername" : externalUsername,
+				"ae:externalUserPassword" :externalPassword,
+				"ae:username" : username_traffic,
+				"ae:password" : password_traffic
+			}
+	};
+	var servicePath= '/wstraffic/services/FFULookupInfoService';
+	var _soapEnvNS=soapEnvNS+ 'xmlns:ae="http://ae:client.ws.ffu.traffic.services.internet.ae"';
+	//var parameters = [envHeader, params, '', _soapEnvNS];
+	var parameters = [JSON.stringify(envHeader),params.toString(), '', _soapEnvNS.toString()];
+	var request = buildBody(parameters, false);
+
+	//Log("FFULookupInfoService request >> " + request);
+	return invokeWebService(request, servicePath, null, isEncryptResponse, encryptionPassword);
+	
 }
 
 function getKioskTestCentersService(params, isEncryptResponse, encryptionPassword) {
@@ -49,31 +70,42 @@ function getKioskTestCentersService(params, isEncryptResponse, encryptionPasswor
 	};
 	var servicePath = "/ws/services/GetKioskTestCentersService";
 	var _soapEnvNS=soapEnvNS+ 'xmlns:ae="http://ae.gov.trf.stp.ws.GetKioskTestCentersService"';
-	var parameters = [envHeader.toString(), params.toString(), 'xmlns:ae="http://ws.trs.rta.ae"', _soapEnvNS.toString()];
+	//var parameters = [envHeader.toString(), params.toString(), 'xmlns:ae="http://ws.trs.rta.ae"', _soapEnvNS.toString()];
+	var parameters = [JSON.stringify(envHeader),params.toString(), 'xmlns:ae="http://ws.trs.rta.ae"', _soapEnvNS.toString()];
 	var request = buildBody(parameters, false);
 
 	//Log("getKioskTestCentersService request >> " + request);
 	return invokeWebService(request,servicePath, null, isEncryptResponse, encryptionPassword);
 }	
 function newMaintenanceService(params, isEncryptResponse, encryptionPassword) {
+    MFP.Logger.info("newMaintenanceService params "+params );
 	var _soapEnvNS=soapEnvNS+ 'xmlns:new="http://dubaipolice/ebsrv/services/NewMaintenance" xmlns:main="http://dubaipolice/ebsrv/services/NewMaintenance"';
 	var envHeader = {
 			"new:externalPassword": externalPassword,
 			"new:externalUsername": externalUsername,
 			"new:password": password,
 			"new:username": userName
-	};	
-	var servicePath = "/ws/services/NewMaintenanceService";
+	};
+   /* var envHeader = {
+            "new:externalPassword": "Test@1234",
+            "new:externalUsername": "mobile_user",
+            "new:password": "m792!du)+1g",
+            "new:username": "Mobstguser"
+    };*/
+    var servicePath = "/ws/services/NewMaintenanceService";
 	//var paramsStr ="[{\"new:getTableData\":{\"RequestName\":\""+params+"\"}}]";
-	//Log("NewMaintenanceService String params >>>>>>>>>>>> " + paramsStr);
+   // MFP.Logger.info("NewMaintenanceService String params >>>>>>>>>>>> " + paramsStr);
 	//params = JSON.stringify(paramsStr);
 	//Log("NewMaintenanceService JSONStringify params >>>>>>>>>>>> " + params);
-	var parameters = [envHeader.toString(), params.toString(), '', _soapEnvNS.toString()];
+	//var parameters = [envHeader.toString(), params.toString(), '', _soapEnvNS.toString()];
+    //var parameters = [JSON.stringify(envHeader), params, '', _soapEnvNS];
+    var parameters =[JSON.stringify(envHeader),params.toString(),'',_soapEnvNS.toString()];
+     //MFP.Logger.info("NewMaintenanceService String parameters >>>>>>>>>>>> " + parameters);
 	var request = buildBody(parameters, false);
-	
+	//var request = buildBody(JSON.stringify(envHeader), params, '', _soapEnvNS, false);
 	Log("NewMaintenanceService request >>>>>>>>>>>> " + request);
-	var response = invokeWebService(request, servicePath, null, isEncryptResponse, encryptionPassword);
-	Log("NewMaintenanceService RESPONSE >>>>>>>>>>>>>>>> " + response);
+    var response = invokeWebService(request, servicePath, null, isEncryptResponse, encryptionPassword);
+	//Log("NewMaintenanceService RESPONSE >>>>>>>>>>>>>>>> " + response);
 	return response;
 	
 }
@@ -88,7 +120,8 @@ function exportCertificateService(params, isEncryptResponse, encryptionPassword)
 	var _soapEnvNS=soapEnvNS+ 'xmlns:ae="http://ae.gov.trf.vhl.ws.ExportCertificateService"';
 
 	var servicePath = "/ws/services/ExportCertificateService";
-	var parameters = [envHeader.toString(), params.toString(), '', _soapEnvNS.toString()];
+	//var parameters = [envHeader.toString(), params.toString(), '', _soapEnvNS.toString()];
+	var parameters = [JSON.stringify(envHeader),params.toString(), '', _soapEnvNS.toString()];
 	var request = buildBody(parameters, false);
 
 	//Log("exportCertificateService request >> " + request);
@@ -104,7 +137,8 @@ function vehiclePossessionCertificateService(params, isEncryptResponse, encrypti
 	};
 	var _soapEnvNS=soapEnvNS+ 'xmlns:urn="urn:VehiclePossessionCertificateService"';
 	var servicePath='/ws/services/VehiclePossessionCertificateService';
-	var parameters = [envHeader.toString(),params.toString(),'', _soapEnvNS.toString()];
+	//var parameters = [envHeader.toString(),params.toString(),'', _soapEnvNS.toString()];
+	var parameters = [JSON.stringify(envHeader),params.toString(), '', _soapEnvNS.toString()];
 	var request = buildBody(parameters, false);
 
 	//Log("VehiclePossessionCertificateService request >> " + request);
@@ -129,69 +163,76 @@ function getServiceTermsAndConditionsService(request, isEncryptResponse, encrypt
 //Log("GetServiceTermsAndConditionsService request >> " + request);
 //return invokeWebService(request,servicePath);
 //}
+
 function buildBody(parameters, isStatic) {
 	var request = "";
-
+   // MFP.Logger.info("drivers_and_vehicles_lookupAdapter buildBody parameters "+parameters );
 	if (isStatic == true) {
 		request = MFP.Server.invokeProcedure({
 			adapter : 'drivers_and_vehciles_utilitiesAdapter',
 			procedure : 'buildBodyFromStaticRequest',
-			parameters : parameters,
+			parameters : parameters
 
 		});
 	} else {
+     //   MFP.Logger.info("drivers_and_vehicles_lookupAdapter going in false ");
 		request = MFP.Server.invokeProcedure({
 			adapter : 'drivers_and_vehciles_utilitiesAdapter',
 			procedure : 'buildBody',
-			parameters : parameters
+			parameters :parameters
+            //parameters : [envHeader,params,'',_soapEnvNS]
 		});
 	}
 
 	return request.body;
+	
+	//return request;
 }
 
-function invokeWebService(body,targetURL,headers, isEncryptResponse, encryptionPassword) {
+function invokeWebService(body, servicePath, headers, isEncryptResponse, encryptionPassword) {
 	var startTime = new Date().getTime();
-	if (!headers)
-		headers = {
-			"SOAPAction" : ""
-	};
-	else
-		headers["SOAPAction"] = "";
-	var input = {
-			method : 'post',
-			returnedContentType : 'xml',
-			path : targetURL,
-			//path:'/TransactionServiceMock',
-			body : {
-				content : body.toString(),
-				contentType : 'text/xml; charset=utf-8'
-			}
-	};
+    if (!headers)
+        headers = {
+            "SOAPAction" : ""
+        };
+    else
+        headers["SOAPAction"] = "";
+    var input = {
+        method : 'post',
+        returnedContentType : 'xml',
+        path : servicePath,
+        body : {
+            content : body.toString(),
+            contentType : 'text/xml; charset=utf-8'
+        }
+    };
 
-	// Adding custom HTTP headers if they were provided as parameter to the
-	// procedure call
-	headers && (input['headers'] = headers);
+    // Adding custom HTTP headers if they were provided as parameter to the
+    // procedure call
+    headers && (input['headers'] = headers);
 
-	var webServiceResult = MFP.Server.invokeHttp(input);
-	if(isEncryptResponse != undefined && isEncryptResponse == true)
-	{
-		var responseString = JSON.stringify(webServiceResult);
-		var invocationData = {
-				adapter : 'drivers_and_vehciles_utilitiesAdapter',
-				procedure : 'encryptData',
-				parameters : [responseString,encryptionPassword]
-		};
-		webServiceResult = MFP.Server.invokeProcedure(invocationData);
-	}	
-	var endTime = new Date().getTime();
-	//Log("time for "+ targetURL + " is " + (endTime - startTime) + " ms");
-	var invocationData = {
-			adapter : 'drivers_and_vehciles_utilitiesAdapter',
-			procedure : 'deleteCredientails',
-			parameters : [webServiceResult]
-	};
-	return MFP.Server.invokeProcedure(invocationData); 
+    var webServiceResult = MFP.Server.invokeHttp(input);
+	
+//	Log("Got back WebService Result >>>>>>>>>>>> ====================== " + webServiceResult);
+	
+    if(isEncryptResponse != undefined && isEncryptResponse == true)
+    {
+        var responseString = JSON.stringify(webServiceResult);
+        var invocationData = {
+            adapter : 'drivers_and_vehciles_utilitiesAdapter',
+            procedure : 'encryptData',
+            parameters : [responseString,encryptionPassword]
+        };
+        webServiceResult = MFP.Server.invokeProcedure(invocationData);
+    }
+    var endTime = new Date().getTime();
+    //Log("time for " + servicePath + " is " + (endTime - startTime) + " ms");
+    var invocationData = {
+            adapter : 'drivers_and_vehciles_utilitiesAdapter',
+            procedure : 'deleteCredientails',
+            parameters : [webServiceResult]
+    };
+    return MFP.Server.invokeProcedure(invocationData);
 }
 
 function invokeWebServiceStatic(request, servicePath, isEncryptResponse, encryptionPassword) {
