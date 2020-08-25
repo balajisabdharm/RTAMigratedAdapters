b06a49922fa001992ee04715a875a2afbb75da00
Omar.Bouchaala <omar.bouchaala@proxym-it.com>
2020-08-25 15:32:13 +0100

    update params to params[0]

--- Adapters/drivers_and_vehicles_customerValidationAdapter/src/main/adapter-resources/js/drivers_and_vehicles_customerValidationAdapter-impl.js
+++ Adapters/drivers_and_vehicles_customerValidationAdapter/src/main/adapter-resources/js/drivers_and_vehicles_customerValidationAdapter-impl.js
@@ -165,8 +165,9 @@ function documentValidation(params, isEncryptResponse, encryptionPassword){
 	var _soapEnvNS = soapEnvNS+ 'xmlns:rta="rta:DocumentValidationService"';
 	//MFP.Logger.warn(params);
 	//var parameters = [envHeader.toString(), params.toString(), "", _soapEnvNS.toString()];
-	//params[0] = JSON.stringify(params[0]);
-	var parameters = [JSON.stringify(envHeader),params.toString(), '', _soapEnvNS.toString()];
+	params[0] = JSON.stringify(params[0]);
+    MFP.Logger.warn(params);
+	var parameters = [JSON.stringify(envHeader),params[0], '', _soapEnvNS.toString()];
 	var request = buildBody(parameters, false);
 	//MFP.Logger.warn("request to be sent:\n"+request);
 	var servicePath='/ws/services/DocumentValidationService';
