b1df441a7c329a2243276682ef48725268dbaf08
fjchawla <69506524+fjchawla@users.noreply.github.com>
2020-08-27 12:44:35 +0400

    added log

--- Adapters/drivers_and_vehicles_lookupsAdapter/src/main/adapter-resources/js/drivers_and_vehicles_lookupsAdapter-impl.js
+++ Adapters/drivers_and_vehicles_lookupsAdapter/src/main/adapter-resources/js/drivers_and_vehicles_lookupsAdapter-impl.js
@@ -105,7 +105,7 @@ function newMaintenanceService(params, isEncryptResponse, encryptionPassword) {
 	//var request = buildBody(JSON.stringify(envHeader), params, '', _soapEnvNS, false);
 	Log("NewMaintenanceService request >>>>>>>>>>>> " + request);
     var response = invokeWebService(request, servicePath, null, isEncryptResponse, encryptionPassword);
-	//Log("NewMaintenanceService RESPONSE >>>>>>>>>>>>>>>> " + response);
+	Log("NewMaintenanceService RESPONSE >>>>>>>>>>>>>>>> " + JSON.stringify(response));
 	return response;
 	
 }
