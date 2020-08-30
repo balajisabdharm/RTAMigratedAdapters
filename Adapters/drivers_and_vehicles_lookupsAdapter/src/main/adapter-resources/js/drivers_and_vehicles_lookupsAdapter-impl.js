e595e2a1d8f56e25512ec743b7ec2d3c3316c656
fjchawla <69506524+fjchawla@users.noreply.github.com>
2020-08-31 00:20:49 +0400

    Update drivers_and_vehicles_lookupsAdapter-impl.js

--- Adapters/drivers_and_vehicles_lookupsAdapter/src/main/adapter-resources/js/drivers_and_vehicles_lookupsAdapter-impl.js
+++ Adapters/drivers_and_vehicles_lookupsAdapter/src/main/adapter-resources/js/drivers_and_vehicles_lookupsAdapter-impl.js
@@ -175,7 +175,7 @@ function buildBody(parameters, isStatic) {
 }
 
 function invokeWebService(body, servicePath, headers, isEncryptResponse, encryptionPassword) {
-	MFP.Logger.info("body:" + JSON.stringify(body));
+	MFP.Logger.info("--------body:" + body.toString());
 	MFP.Logger.info("servicePath:" + JSON.stringify(servicePath));
 	MFP.Logger.info("headers:" + JSON.stringify(headers));
 	MFP.Logger.info("isEncryptResponse:" + JSON.stringify(isEncryptResponse));
