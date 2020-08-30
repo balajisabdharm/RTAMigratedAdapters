7fa93258445551c168e84c06c4c7207fbdba3152
fjchawla <69506524+fjchawla@users.noreply.github.com>
2020-08-31 03:26:32 +0400

    Update drivers_and_vehicles_lookupsAdapter-impl.js

--- Adapters/drivers_and_vehicles_lookupsAdapter/src/main/adapter-resources/js/drivers_and_vehicles_lookupsAdapter-impl.js
+++ Adapters/drivers_and_vehicles_lookupsAdapter/src/main/adapter-resources/js/drivers_and_vehicles_lookupsAdapter-impl.js
@@ -40,7 +40,7 @@ function FFULookupInfoService(params, isEncryptResponse, encryptionPassword){
 	var invocationData = {
 			adapter : 'drivers_and_vehicles_trafficAdapter',
 			procedure : 'FFULookupInfoService',
-			parameters : [params, isEncryptResponse, encryptionPassword]
+			parameters : [JSON.stringify(params), isEncryptResponse, encryptionPassword]
 	};
 	var response =  MFP.Server.invokeProcedure(invocationData);
 	
