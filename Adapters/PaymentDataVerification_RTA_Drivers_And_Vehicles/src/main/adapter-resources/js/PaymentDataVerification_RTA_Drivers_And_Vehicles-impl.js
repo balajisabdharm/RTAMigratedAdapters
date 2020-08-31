41a5f04cd5bd9744f95d447720a94e77b9ff956c
fjchawla <69506524+fjchawla@users.noreply.github.com>
2020-09-01 01:20:38 +0400

    Update PaymentDataVerification_RTA_Drivers_And_Vehicles-impl.js

--- Adapters/PaymentDataVerification_RTA_Drivers_And_Vehicles/src/main/adapter-resources/js/PaymentDataVerification_RTA_Drivers_And_Vehicles-impl.js
+++ Adapters/PaymentDataVerification_RTA_Drivers_And_Vehicles/src/main/adapter-resources/js/PaymentDataVerification_RTA_Drivers_And_Vehicles-impl.js
@@ -75,13 +75,6 @@ function getTransactionAmount(spTRN)
 	return amount;
 }
 
-function encryptData(data,appId) {
-	var password = MFP.Server.getPropertyValue("epay.Encryption.PASSWORD.RTA" + appId);
-	
-	var cypherText = Aes.Ctr.encrypt(data,password,ENCRYPTION_STRENGTH);
-	//return {cypherText: cypherText };
-	return {cypherText: password};
-}
 
 
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  */
@@ -98,3 +91,10 @@ var ENCRYPTION_STRENGTH = MFP.Server.getPropertyValue("epay.Encryption.STRENGTH"
 if (ENCRYPTION_STRENGTH == '256') ENCRYPTION_STRENGTH = 256;
 else ENCRYPTION_STRENGTH = 128;
 
+function encryptData(data,appId) {
+	var password = MFP.Server.getPropertyValue("epay.Encryption.PASSWORD.RTA" + appId);
+	
+	var cypherText = Aes.Ctr.encrypt(data,password,ENCRYPTION_STRENGTH);
+	//return {cypherText: cypherText };
+	return {cypherText: password};
+}
