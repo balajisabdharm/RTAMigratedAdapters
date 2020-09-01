11bbec3ef894a9af9a1057c580e90371f274e8de
fjchawla <69506524+fjchawla@users.noreply.github.com>
2020-09-01 06:30:53 +0400

    Update drivers_and_vehciles_utilitiesAdapter_Tibco-impl.js

--- Adapters/drivers_and_vehciles_utilitiesAdapter_Tibco/src/main/adapter-resources/js/drivers_and_vehciles_utilitiesAdapter_Tibco-impl.js
+++ Adapters/drivers_and_vehciles_utilitiesAdapter_Tibco/src/main/adapter-resources/js/drivers_and_vehciles_utilitiesAdapter_Tibco-impl.js
@@ -438,7 +438,7 @@ function buildBody(envHeader, params, namespaces, soapEnvNS) {
 
 function buildBody2(envHeader, params, namespaces, soapEnvNS) {
 	var body = '<soapenv:Envelope ' + soapEnvNS + '>\n'+ '<soapenv:Header>\n'+ ' <wsse:Security soapenv:mustUnderstand="1" xmlns:wsse="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd" xmlns:wsu="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd"> \n'+ ' <wsse:UsernameToken wsu:Id="UsernameToken-8">  \n';
-
+	MFP.Logger.info("build Body 2 Called");
 	body = jsonToXml(envHeader, body, namespaces);
 	//body = jsonToXml(JSON.parse(envHeader), body.toString(), namespaces);
 	body += '</wsse:UsernameToken>\n'+ '</wsse:Security>\n'+ '</soapenv:Header>\n';
