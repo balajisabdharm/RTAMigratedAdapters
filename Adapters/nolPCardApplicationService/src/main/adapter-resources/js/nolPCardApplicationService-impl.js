574488fa144e37f89fe7b67403e175d3668e6d08
Balaji Sabdhar M <bmuthukr@in.ibm.com>
2020-09-02 12:08:33 +0530

    Issue Fix

--- Adapters/nolPCardApplicationService/src/main/adapter-resources/js/nolPCardApplicationService-impl.js
+++ Adapters/nolPCardApplicationService/src/main/adapter-resources/js/nolPCardApplicationService-impl.js
@@ -149,7 +149,7 @@ function getWSSE(){
 	var wsse = 
 		'<wsse:Security soapenv:mustUnderstand="0" xmlns:wsse="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd" xmlns:wsu="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd">'
 			+'<wsse:UsernameToken wsu:Id="UsernameToken-69">'
-				+'<wsse:Username>'+ MFP.Server.getPropertyValue("wsse.tibco.username"])+'</wsse:Username>'
+				+'<wsse:Username>'+ MFP.Server.getPropertyValue("wsse.tibco.username")+'</wsse:Username>'
 				+'<wsse:Password Type="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-username-token-profile-1.0#PasswordText">'+ MFP.Server.getPropertyValue("wsse.tibco.password")+'</wsse:Password>'
 				+'<wsu:Created>'+new Date().toISOString()+'</wsu:Created>'
 			+'</wsse:UsernameToken>'
