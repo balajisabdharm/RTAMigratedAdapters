132be045d9b7a5a261342b0dc1678cd0050e6375
fjchawla <69506524+fjchawla@users.noreply.github.com>
2020-09-02 16:28:00 +0400

    Update drivers_and_vehicles_SalikTokenAdapter-impl.js

--- Adapters/drivers_and_vehicles_SalikTokenAdapter/src/main/adapter-resources/js/drivers_and_vehicles_SalikTokenAdapter-impl.js
+++ Adapters/drivers_and_vehicles_SalikTokenAdapter/src/main/adapter-resources/js/drivers_and_vehicles_SalikTokenAdapter-impl.js
@@ -151,10 +151,11 @@ function fixNameSpaceGenTok(response){
 	MFP.Logger.info(" ================================================= REMOVING NAMESPACE 2 =================================================");
 	var newResponse = JSON.stringify(response);
 	var reg1 = new RegExp('"":"http://www.rta.ae/schemas/SalikTokenGenerationService/Schema.xsd",', "g");
-	var reg2 = new RegExp('"CDATA":',"g");
+	var reg2 = new RegExp('{"CDATA":',"g");
 	var reg3 = new RegExp('"},"',"g");
+	var reg4 = new RegExp('"}}}',"g");
 	
-	newResponse = newResponse.replace(reg1,"").replace(reg2,"").replace(reg3,"\",\"");
+	newResponse = newResponse.replace(reg1,"").replace(reg2,"").replace(reg3,"\",\"").replace(reg4,"}}");
 
 	MFP.Logger.info("refined Response -->" + newResponse);
 	try{
